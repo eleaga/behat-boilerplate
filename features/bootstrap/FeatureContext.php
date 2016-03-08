1ea97a225ee793b31d8d4a419d674aeeb6b60a66
luiz bezerra <luiz.bezerra@itelios.com>
2016-03-08 16:45:09 -0300

    first commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,24 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * @When I wait for :arg1 second(s)
+     */
+    public function iWaitForSecond($seconds)
+    {
+        sleep($seconds);
+    }
+}
---
+++ features/eni.feature
@@ -0,0 +1,5 @@
+Feature: Home
+
+Scenario: Run a test
+	Given I am on "/"
+	Then I should see "Welcome"
